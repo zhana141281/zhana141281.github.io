52333251db0320a08265dbf489242086e32ea771
@zhana141281 <zhana141281@gmail.com>
2019-08-02 15:44:17 +0300

    add folders
    add folders

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+
+let dropDown = document.querySelector('.navbar__dropmenu');
+let dropItem = document.querySelector('.navbar__menu-line');
+dropItem.addEventListener('click', function () {
+    dropDown.classList.toggle('active')});
+dropDown.addEventListener('click', function () {
+    dropDown.classList.toggle('active')});
