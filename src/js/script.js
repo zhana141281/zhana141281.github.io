c03f69d4bc24196f058639f2b415f23e2fb4d506
@zhana141281 <zhana141281@gmail.com>
2019-08-04 22:07:45 +0300

    4/07/19

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,12 @@
 
 let dropDown = document.querySelector('.navbar__dropmenu');
 let dropItem = document.querySelector('.navbar__menu-line');
+let header = document.querySelector('.header');
+
 dropItem.addEventListener('click', function () {
     dropDown.classList.toggle('active')});
 dropDown.addEventListener('click', function () {
     dropDown.classList.toggle('active')});
+
+
+
